3fd90d0d9870a9dff6e41dc956748e040b789385
Nikson Diniz <nikson.diniz@gmail.com>
2023-10-14 10:08:08 -0300

    Service layer, component registration

--- src/main/java/com/ndiniz/course/services/UserService.java
+++ src/main/java/com/ndiniz/course/services/UserService.java
@@ -1,5 +1,26 @@
 package com.ndiniz.course.services;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ndiniz.course.entities.User;
+import com.ndiniz.course.repositories.UserRepository;
+
+@Service
 public class UserService {
-	
+
+	@Autowired
+	private UserRepository repository;
+
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
 }
