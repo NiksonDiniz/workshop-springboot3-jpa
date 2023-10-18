2a3315899beda63a5e9c6b390223416a91786a33
Nikson Diniz <nikson.diniz@gmail.com>
2023-10-17 21:06:36 -0300

    Product-OrderItem one-to-many association

--- src/main/java/com/ndiniz/course/entities/OrderItem.java
+++ src/main/java/com/ndiniz/course/entities/OrderItem.java
@@ -10,7 +10,6 @@ import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 
-
 @Entity
 @Table(name = "tb_order_item")
 public class OrderItem implements Serializable {
@@ -18,7 +17,7 @@ public class OrderItem implements Serializable {
 
 	@EmbeddedId
 	private OrderItemPK id = new OrderItemPK();
-	
+
 	private Integer quantity;
 	private Double price;
 
