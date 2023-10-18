a8f6573d06b74de45f4c5dfa05c1bdf1fbc4748e
Nikson Diniz <nikson.diniz@gmail.com>
2023-10-17 21:56:24 -0300

    Subtotal & Total methods

--- src/main/java/com/ndiniz/course/entities/Order.java
+++ src/main/java/com/ndiniz/course/entities/Order.java
@@ -101,6 +101,14 @@ public class Order implements Serializable {
 		}
 	}
 
+	public Double getTotal() {
+		double sum = 0.0;
+		for (OrderItem x : items) {
+			sum += x.getSubtotal();
+		}
+		return sum;
+	}
+
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
